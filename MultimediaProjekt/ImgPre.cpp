445e8dfc224ce6625c7200f9576d0c2ff68770d5
Fabian Rabe <rabe.nimmermehr@gmail.com>
2013-07-08 10:52:17 +0200

    ImgPre Rumpf erzeugt

---
+++ MultimediaProjekt/ImgPre.cpp
@@ -0,0 +1,24 @@
+#include "ImgPre.h"
+#include <stdio.h>
+#include <array>
+
+
+namespace ImgPre {
+	using namespace std;
+	//IMAGELOADER
+
+	void loadImagesFromPackage(std::string folderPath, cv::Mat loadedImages[]){
+	
+	}
+
+	void getLoadedImages(cv::Mat loadedImages[]){
+
+	}
+
+	//IMAGESCALER
+
+	void scaleImageToScaledArray(Image input_image, ImageArray scaled_images){
+
+	}
+
+}
